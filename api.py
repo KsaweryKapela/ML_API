2341d3da7f5e627a0e4dc59ea5afecc4e23f7825
KsaweryKapela <ksawerykapela97@gmail.com>
2023-04-18 23:46:39 +0200

    Packed repeated code in function

--- api.py
+++ api.py
@@ -8,15 +8,22 @@ from clf_models.heuristic_classification import SimpleHeuristicModel
 
 app = Flask(__name__)
 
-@app.route("/shallow/<model>", methods=['POST'])
-def return_shallow_model(model):
 
+def read_features():
     requested_data = request.get_json()
     features = requested_data['features']
 
-    # Preparing 2d arr, even if there's if sample == 1
+     # Makes sure features are 2d, even if sample == 1
     if not isinstance(features[0], list):
         features = [features]
+
+    return features
+
+
+@app.route("/shallow/<model>", methods=['POST'])
+def return_shallow_model(model):
+
+    features = read_features()
 
     if model == 'rfc':
 
@@ -38,13 +45,11 @@ def return_shallow_model(model):
 @app.route("/deep", methods=['POST'])
 def return_deep_model():
 
-    requested_data = request.get_json()
-    features = requested_data['features']
-
-    if not isinstance(features[0], list):
-        features = [features]
+    features = read_features()
 
     NN_model = keras.models.load_model('clf_models/models/NN_clf')
+
+    # Decoding from one hot encoding to int
     NN_pred = NN_model.predict(features)
     NN_pred = np.round(NN_pred).argmax(axis=1)
     NN_pred += 1
@@ -55,11 +60,7 @@ def return_deep_model():
 @app.route("/heuristic", methods=['POST'])
 def return_heuristic_model():
 
-    requested_data = request.get_json()
-    features = requested_data['features']
-
-    if not isinstance(features[0], list):
-        features = [features]
+    features = read_features()
     
     heuristic_model = SimpleHeuristicModel()
     heuristic_pred = heuristic_model.predict(features)
